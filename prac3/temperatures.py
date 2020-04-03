840f819f4b2c984428ad2423bae354edead82bcf
Andy <andywijaya1717@gmail.com>
2020-04-03 17:54:20 +0800

    Add temperatures.py Celsius to fahrenheit and fahrenheit to celsius

---
+++ prac3/temperatures.py
@@ -0,0 +1,16 @@
+"""
+Celsius to Fahrenheit
+"""
+
+def celsius_to_fahrenheit(temperature):
+    fahrenheit=(temperature*1.8)+32
+    return fahrenheit
+
+"""
+Fahrenheit to Celsius
+"""
+
+def fahrenheit_to_celsius(temperature):
+    celsius=(temperature-32)*5/9
+    return celsius
+
