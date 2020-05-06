d0abfffb2e4e07ce4f821d072a622f38820e145f
Andy <andywijaya1717@gmail.com>
2020-05-06 15:02:26 +0800

    Finished all prac

---
+++ prac6/used.cars.py
@@ -0,0 +1,15 @@
+from prac6.car import car
+
+
+def main():
+    my_car = Car(180)
+    my_car.drive(30)
+    print("fuel =", my_car.fuel)
+    print("odo =", my_car.odometer)
+    print(my_car)
+
+    print("Car {}, {}".format(my_car.fuel, my_car.odometer))
+    print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+
+
+main()
