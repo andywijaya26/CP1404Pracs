2bdb495cb1d12dc1845b61371c2d15851f2b7112
Andy <andywijaya1717@gmail.com>
2020-05-20 19:29:04 +0800

    Finished practical 7

---
+++ prac8/silver_service_taxi_test.py
@@ -0,0 +1,11 @@
+from prac8.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    """Test SilverServiceTaxi."""
+    taxi = SilverServiceTaxi("Test Fancy Taxi", 100, 2)
+    taxi.drive(18)
+    print(taxi)
+    print(taxi.get_fare())
+
+main()
